d8cecd8b12cee5a874b482f4264c4cd642ae1978
Dany Diaz <dany.diaz91@hotmail.com>
2024-06-15 16:45:11 -0500

    added blocks and transactions

---
+++ core/block_test.go
@@ -0,0 +1,64 @@
+package core
+
+import (
+	"bytes"
+	"github.com/danydiaz91/go-blockchain/types"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestHeader_Encode_Decode_Binary(t *testing.T) {
+	h := &Header{
+		Version:   1,
+		PrevBlock: types.RandomHash(),
+		Timestamp: time.Now().UnixNano(),
+		Height:    10,
+		Nonce:     989395,
+	}
+
+	buf := &bytes.Buffer{}
+	assert.Nil(t, h.EncodeBinary(buf))
+
+	hDecode := &Header{}
+	assert.Nil(t, hDecode.DecodeBinary(buf))
+
+	assert.Equal(t, h, hDecode)
+}
+
+func TestBlock_Encode_Decode_Binary(t *testing.T) {
+	b := &Block{
+		Header: Header{
+			Version:   1,
+			PrevBlock: types.RandomHash(),
+			Timestamp: time.Now().UnixNano(),
+			Height:    10,
+			Nonce:     989395,
+		},
+		Transactions: nil,
+	}
+
+	buf := &bytes.Buffer{}
+	assert.Nil(t, b.EncodeBinary(buf))
+
+	bDecode := &Block{}
+	assert.Nil(t, bDecode.DecodeBinary(buf))
+
+	assert.Equal(t, b, bDecode)
+}
+
+func TestBlock_Hash(t *testing.T) {
+	b := Block{
+		Header: Header{
+			Version:   1,
+			PrevBlock: types.RandomHash(),
+			Timestamp: time.Now().UnixNano(),
+			Height:    10,
+			Nonce:     989395,
+		},
+		Transactions: nil,
+	}
+
+	h := b.Hash()
+	assert.False(t, h.IsZero())
+}
